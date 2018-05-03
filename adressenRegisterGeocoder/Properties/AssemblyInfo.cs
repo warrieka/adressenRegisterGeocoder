5e96e2c96290a85e7728483ef0e2fb10626382fc
Kay Warrie <kaywarrie@gmail.com>
2018-05-03 18:11:30 +0200

    dotnet version check
    Former-commit-id: 20b1e1cbf96c4e19def343bf56ce6562b3a48c8c

--- adressenRegisterGeocoder/Properties/AssemblyInfo.cs
+++ adressenRegisterGeocoder/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stad Antwerpen")]
 [assembly: AssemblyProduct("adressenRegisterGeocoder")]
-[assembly: AssemblyCopyright("Copyright © Digipolis 2018")]
+[assembly: AssemblyCopyright("Copyright © Kay Warrie 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("nl")]
